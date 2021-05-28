d96f4230da6135468396f1baf17331a2e13156e2
yonle <fledyonle@gmail.com>
2021-05-28 15:07:36 +0800

    Initial

---
+++ README.md
@@ -0,0 +1,44 @@
+# AFKBot
+A well, Very simple AFK bot for discord that written in [NodeJS](https://nodejs.org) with [Discord.js](https://discord.js.org) Library.
+
+## Story of this project
+First time, I think writting a AFK bot code is very hard to do in Discord.js, But i'm wrong, I think multiple time how to do it and finally, I made a very simple code line. I hope you guys enjoy it.
+
+## Setting everything up
+```bash
+git clone https://github.com/Yonle/AFKBot
+cd AFKBot
+npm install --save-optional
+```
+
+## Configuration
+For [**Heroku**](https://heroku.com) user, Go to **App Settings** then add **TOKEN** in **Config Vars** section with your bot token as the value.
+![Putting Token in Heroku](https://media.discordapp.net/attachments/839047579512602681/847729094313508904/Screenshot_2021_0528_145129.png)
+
+For self-host user, Install `dotenv` package and fill all required fields in `.env.example` files. Then rename the file as `.env`
+```
+# Isn't that simple? 0 Third party, Say what now? =)
+# Rename this file as .env after adding your token. Then run after your job done, Good luck....
+
+TOKEN=
+```
+
+## Running this bot
+```bash
+node index.js
+# Or
+npm start
+```
+
+## Database Support
+This repo is using [`Map()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map). To get a experience of full database support, Use [enmap](https://enmap.evie.dev/)
+
+## Community
+- Discord
+  - [My Discord](https://discord.gg/9S3ZCDR)
+  - [Termux Discord](https://discord.gg/gF3gSgwUqw)
+  - [Glitch.com Unofficial Discord](https://discord.gg/6CWnE6W)
+- Telegram
+  - [My Telegram Group](https://t.me/yonlecoder)
+  - [Termux Group](https://t.me/termux24x7)
+  - [Glitch.com Unofficial Group](https://t.me/glitchdotcom_community)
---
+++ index.js
@@ -0,0 +1,55 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+const afk = new Map();
+
+// We're gonna load dotenv, Yet
+try {
+	let dotenv = require("dotenv");
+	dotenv.config();
+} catch (error) {
+	// Do nothing. Because dotenv is Optional Dependencies.
+}
+
+// Debug Logging
+// Only required if ya want know your bot status
+// Not including received message and etc :^)
+bot.on('debug', console.log);
+bot.on('message', (message) => {
+	let reason = message.content.split(' ').slice(1).join(' ');
+	let mention = message.mentions.users.first();
+	let author = message.author;
+
+	if (author.id === bot.user.id || message.webhookID || message.channel.type != "text") return;
+	if (mention && mention.id == bot.user.id && !afk.has(author.id) && !reason) {
+		return message.channel.send(`Heyya. To set your status as AFK user, Mention me with the reason. Example: ${bot.user} Studying`).then(m => {
+			setTimeout(() => m.delete().catch(() => {}), 1000 * 10);
+		});
+	} else if (mention && mention.id == bot.user.id && reason.length && !afk.has(author.id)) {
+		afk.set(author.id, reason);
+		return message.reply("You're now AFK.", {
+			embed: {
+				color: 1048575,
+				description: reason
+			}
+		});
+	} else if (mention && afk.has(mention.id)) {
+		message.reply(`${mention} is Busy right now.`, {
+			embed: {
+				color: 16773120,
+				description: afk.get(mention.id)
+			}
+		});
+	} else if (afk.has(author.id)) {
+		return message.channel.send(`Welcome back, ${author}. I've been removed your AFK.`, {
+			embed: {
+				color: 1048575,
+				title: "Last AFK Reason:",
+				description: afk.get(author.id)
+			}
+		}).then(() => afk.delete(author.id));
+	}
+});
+
+bot.login(process.env.TOKEN);
+
+process.on('unhandledRejection', console.error);
